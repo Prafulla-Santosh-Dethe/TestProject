177d7c1fcb09f5cee8ca261b09115972d792e4f0
Prafulla_Dethe <dethe27999@gmail.com>
2022-07-24 08:57:20 +0530

    Addition & Substraction code typed

--- TestProject/src/Demo.java
+++ TestProject/src/Demo.java
@@ -1,4 +1,14 @@
 
-public class Demo {
+public class Demo 
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		System.out.println("Addition= "+(a+b));
+		System.out.println("Substraction= "+(a-b));
+		
+		
+	}
 
 }
